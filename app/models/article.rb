ecca2b6836593f4d6ed9e0ebcde39c5a4a30fb6c
James Wilhelm <jwilhelm@Jamess-MacBook-Air.local>
2020-08-25 21:12:43 -0400

    add one-to-many association between user and articles, back-end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     #this will make sure title is present to be saved
     validates :title, presence: true, length: {minimum: 6, maximum:100}
     validates :description, presence: true, length: {minimum:10, maximum: 300}
