4212e36f9d8642c50fe8fc792bcdd6f6b2e3fc45
James Wilhelm <jwilhelm@Jamess-MacBook-Air.local>
2020-09-06 16:32:31 -0400

    made it so users show as logged in an unable to sign-up again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    #helper method makes it viewable by controller and views
+    helper_method :current_user
+    def current_user
+        #checks for current user fist then does the other if not found
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    helper_method :logged_in?
+    def logged_in?
+        #turn current user too boolean
+        !!current_user
+    end
+
+
 end
